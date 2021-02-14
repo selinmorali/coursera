b0fea3b9e9ac31d67e6a317629e271d49cc9275b
selinmorali <73489969+selinmorali@users.noreply.github.com>
2021-02-14 17:22:55 +0300

    Update script.js

--- Module4Solution/script.js
+++ Module4Solution/script.js
@@ -1,5 +1,5 @@
 (function (window) {
-  var names = ["John", "Jarry", "Jack", "Anna", "Barney", "Dennis", "Ed", "Mark", "Bob", "Paula", "Zack"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   for (var i in names) {
     var firstLetter = names[i].charAt(0).toLowerCase();
